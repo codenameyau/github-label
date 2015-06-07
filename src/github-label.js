2d1e176b3e7ac9fa7e12f05e8c12ef8942ea474e
Jorge Yau <jyau01@citymail.cuny.edu>
2015-06-07 18:41:17 -0400

    #4 - Handling credentials for authentication

--- src/github-label.js
+++ src/github-label.js
@@ -5,9 +5,14 @@
  */
 'use strict';
 
+// Dependencies.
 var requireDir = require('require-dir');
 var program = require('commander');
+var prompt = require('prompt');
 var github = require('octonode');
+var fs = require('fs');
+
+// JSON and constants.
 var pjson = require('../package.json');
 var presetLabels = requireDir('./presets');
 var ACCESS_TOKEN = process.env.GITHUB_LABEL_TOKEN;
@@ -16,9 +21,40 @@ var ACCESS_TOKEN = process.env.GITHUB_LABEL_TOKEN;
 /********************************************************************
 * INTERNAL FUNCTIONS
 *********************************************************************/
+
+// Pass if repository, key, user, and pw
+var postGithubLabels = function(labels) {
+
+};
+
 var _exitMessage = function(msg) {
   console.log(msg);
   process.exit(1);
+};
+
+var _readUserLabels = function(filepath) {
+  fs.readFile(filepath, function(error, data) {
+    if (error) {
+      _exitMessage('Could not read file: ' + filepath);
+    }
+
+    // Let GitHub handle the validation.
+    postGithubLabels(JSON.parse(data));
+  }, 'utf8');
+};
+
+var _getAuthCredentials = function(callback) {
+  var authPrompt = [
+    { name: 'username' },
+    { name: 'password', hidden: true }
+  ];
+
+  prompt.start();
+  prompt.get(authPrompt, function(error, result) {
+    if (error) {
+      _exitMessage('Exit: invalid login credentials.');
+    } callback(results);
+  });
 };
 
 
@@ -26,13 +62,12 @@ var _exitMessage = function(msg) {
 * MAIN PROGRAM
 *********************************************************************/
 program.version(pjson.version)
-  .option('-u, --username [value]', 'Specify your GitHub username.')
   .option('-r, --repository [value]', 'Specify a GitHub repository.')
   .option('-p, --preset [value]', 'Specify a labels preset.')
   .option('-j, --json [value]', 'Specify your own JSON label preset.')
   .parse(process.argv);
 
-// Validate that options are specified.
+// Show help if no options are specified.
 if (program.rawArgs.length < 3) {
   program.help();
 }
@@ -43,13 +78,17 @@ if (!program.repository) {
 }
 
 // Validate that either acess token or username is specified.
-if (!ACCESS_TOKEN && !program.user) {
-  _exitMessage('option: -u, --username is required.');
+if (!ACCESS_TOKEN) {
+  _getAuthCredentials();
 }
 
 // Grab label preset by default.
 var presetName = program.option.preset || 'default';
-var labels = presetLabels[presetName];
 
-console.log(program.repository);
-console.log(labels);
+// Use user's JSON label preset if specified.
+if (program.json) {
+  _readUserLabels(program.json);
+} else {
+  var labels = presetLabels[presetName];
+  postGithubLabels(labels);
+}
