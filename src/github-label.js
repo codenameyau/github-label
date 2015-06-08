a1ae9e29457beaa25985163072ba5f229ea1b8ef
Jorge Yau <jyau01@citymail.cuny.edu>
2015-06-07 23:51:54 -0400

    #2 - Adding user json loader for labels

--- src/github-label.js
+++ src/github-label.js
@@ -5,90 +5,107 @@
  */
 'use strict';
 
-// Dependencies.
 var requireDir = require('require-dir');
 var program = require('commander');
 var prompt = require('prompt');
 var github = require('octonode');
 var fs = require('fs');
-
-// JSON and constants.
 var pjson = require('../package.json');
-var presetLabels = requireDir('./presets');
-var ACCESS_TOKEN = process.env.GITHUB_LABEL_TOKEN;
+var labelPresets = requireDir('./presets');
 
 
 /********************************************************************
-* INTERNAL FUNCTIONS
+* FUNCTION CONSTRUCTOR
 *********************************************************************/
+function GithubCLI() {
+  // Command-line argument parser.
+  this.program = program.version(pjson.version)
+    .arguments('repo')
+    .option('-r, --repository [value]', 'Specify a GitHub repository.')
+    .option('-p, --preset <items>', 'Specify a label preset.')
+    .option('-j, --json [value]', 'Specify your own JSON label preset.')
+    .parse(process.argv);
 
-// Pass if repository, key, user, and pw
-var postGithubLabels = function(labels) {
+  // Setup octonode client.
+  this.ACCESS_TOKEN = process.env.GITHUB_LABEL_TOKEN;
+  this.client = null;
+  this.validateArguments();
+}
 
-};
 
-var _exitMessage = function(msg) {
-  console.log(msg);
+/********************************************************************
+* PUBLIC METHODS
+*********************************************************************/
+GithubCLI.prototype.exit = function(message) {
+  console.log('Exit: ' + message);
   process.exit(1);
 };
 
-var _readUserLabels = function(filepath) {
-  fs.readFile(filepath, function(error, data) {
-    if (error) {
-      _exitMessage('Could not read file: ' + filepath);
-    }
-
-    // Let GitHub handle the validation.
-    postGithubLabels(JSON.parse(data));
-  }, 'utf8');
+GithubCLI.prototype.setupAuthClient = function(username, password) {
+  this.client = github.client({
+    username: username,
+    password: password
+  });
 };
 
-var _getAuthCredentials = function(callback) {
+GithubCLI.prototype.setupTokenClient = function() {
+  this.client = github.client(this.ACCESS_TOKEN);
+};
+
+GithubCLI.prototype.getAuthCredentials = function() {
+  var _this = this;
   var authPrompt = [
-    { name: 'username' },
-    { name: 'password', hidden: true }
+    { name: 'username', type: 'string', required: true },
+    { name: 'password', type: 'string', required: true, hidden: true }
   ];
 
+  console.log('Please enter your GitHub credentials.\n');
   prompt.start();
   prompt.get(authPrompt, function(error, result) {
     if (error) {
-      _exitMessage('Exit: invalid login credentials.');
-    } callback(results);
+      this.exit('invalid login credentials.');
+    } _this.setupAuthClient(result.username, result.password);
   });
+};
+
+GithubCLI.prototype.validateArguments = function() {
+  // Show help if no options are specified.
+  if (this.program.rawArgs.length < 3) {
+    this.program.help();
+  }
+
+  // Validate that repository is specified.
+  if (!this.program.repository) {
+    this.exit('option --repository is required.');
+  }
+
+  // Validate either access token or credentials.
+  this.setupGithubClient();
+};
+
+GithubCLI.prototype.setupGithubClient = function() {
+  if (this.ACCESS_TOKEN) {
+    this.setupTokenClient();
+  } else {
+    this.getAuthCredentials();
+  }
+};
+
+GithubCLI.prototype.getLabels = function() {
+  return labelPresets[this.program.preset];
+};
+
+GithubCLI.prototype.readLabels = function(filepath, cb) {
+  var _this = this;
+  fs.readFile(filepath, function(error, data) {
+    if (error) {
+      _this.exit('could not read file: ' + filepath);
+    } cb(data);
+  }, 'utf8');
 };
 
 
 /********************************************************************
 * MAIN PROGRAM
 *********************************************************************/
-program.version(pjson.version)
-  .option('-r, --repository [value]', 'Specify a GitHub repository.')
-  .option('-p, --preset [value]', 'Specify a labels preset.')
-  .option('-j, --json [value]', 'Specify your own JSON label preset.')
-  .parse(process.argv);
-
-// Show help if no options are specified.
-if (program.rawArgs.length < 3) {
-  program.help();
-}
-
-// Validate that repository is specified.
-if (!program.repository) {
-  _exitMessage('option: -r, --repository is required.');
-}
-
-// Validate that either acess token or username is specified.
-if (!ACCESS_TOKEN) {
-  _getAuthCredentials();
-}
-
-// Grab label preset by default.
-var presetName = program.option.preset || 'default';
-
-// Use user's JSON label preset if specified.
-if (program.json) {
-  _readUserLabels(program.json);
-} else {
-  var labels = presetLabels[presetName];
-  postGithubLabels(labels);
-}
+var cli = new GithubCLI();
